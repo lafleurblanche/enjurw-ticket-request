de7b40b30aebb3a62f3cd931e2ad434a09a23da2
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-06 14:34:11 +0900

    20230606 First Commit

---
+++ enjurw-ticket-request/src/features/RouteSelect/RailwayList/EnjuRWRailwayList01.ts
@@ -0,0 +1,34 @@
+const EnjuRWRailwayList01 = [
+  {
+    id: 1,
+    routeId: 'RW01',
+    routeName1: '苆谷',
+    routeName2: '線'
+  },
+  {
+    id: 2,
+    routeCode: 'RW02',
+    routeName1: '茄谷',
+    routeName2: '線'
+  },
+  {
+    id: 3,
+    routeCode: 'RW03',
+    routeName1: '笥谷',
+    routeName2: '線'
+  },
+  {
+    id: 4,
+    routeCode: 'RW04',
+    routeName1: '筏谷',
+    routeName2: '線'
+  },
+  {
+    id: 5,
+    routeCode: 'RW05',
+    routeName1: '筮谷',
+    routeName2: '線'
+  },
+];
+
+export default EnjuRWRailwayList01;
