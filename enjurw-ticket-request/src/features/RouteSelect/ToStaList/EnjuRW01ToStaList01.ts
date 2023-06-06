ea892db1f27de4266c8887435e23e5e9c2371014
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-06 14:35:33 +0900

    20230606 First Commit

---
+++ enjurw-ticket-request/src/features/RouteSelect/ToStaList/EnjuRW01ToStaList01.ts
@@ -0,0 +1,29 @@
+const EnjuRW01FromStaList01 = [
+  {
+    id: 1,
+    staCode: 'RW0101',
+    staName: '苆谷'
+  },
+  {
+    id: 2,
+    staCode: 'RW0102',
+    staName: '東苆谷'
+  },
+  {
+    id: 3,
+    staCode: 'RW0103',
+    staName: ''
+  },
+  {
+    id: 4,
+    staCode: 'RW0104',
+    staName: ''
+  },
+  {
+    id: 5,
+    staCode: 'RW0105',
+    staName: ''
+  },
+];
+
+export default EnjuRW01FromStaList01;
