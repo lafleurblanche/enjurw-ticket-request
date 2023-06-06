87e500ae58ff59cf747d931c0ee0ab9eacb6b8ca
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-06 22:14:11 +0900

    20230606 NEW

---
+++ enjurw-ticket-request/src/features/RouteSelect/ViaRouteList/ViaRouteListEnjuRW02.ts
@@ -0,0 +1,39 @@
+const ViaRouteListEnjuRW02 = [
+  {
+    id: 0,
+    routeID: 'RW02',
+    routeName: '自線内',
+    viaRouteName: '茄谷',
+    selectedRoute: '経茄'
+  },
+  {
+    id: 1,
+    routeID: 'RW99',
+    routeName: '',
+    viaRouteName: '',
+    selectedRoute: ''
+  },
+  {
+    id: 2,
+    routeID: 'RW99',
+    routeName: '',
+    viaRouteName: '',
+    selectedRoute: ''
+  },
+  {
+    id: 3,
+    routeID: 'RW99',
+    routeName: '',
+    viaRouteName: '',
+    selectedRoute: ''
+  },
+  {
+    id: 4,
+    routeID: 'RW99',
+    routeName: '',
+    viaRouteName: '',
+    selectedRoute: ''
+  },
+];
+
+export default ViaRouteListEnjuRW02;
