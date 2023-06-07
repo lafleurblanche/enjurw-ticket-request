7e1a18c16d8e7fa64855394f9c6a2da60b110816
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-07 19:35:42 +0900

    20230607 ADD

--- enjurw-ticket-request/src/features/RouteSelect/FromStaList/index.ts
+++ enjurw-ticket-request/src/features/RouteSelect/FromStaList/index.ts
@@ -4,3 +4,4 @@ export { default as EnjuRW03FromStaList01 } from './EnjuRW03FromStaList01';
 export { default as EnjuRW04FromStaList01 } from './EnjuRW04FromStaList01';
 export { default as EnjuRW05FromStaList01 } from './EnjuRW05FromStaList01';
 export { default as EnjuRW06FromStaList01 } from './EnjuRW06FromStaList01';
+export { default as EnjuRW07FromStaList01 } from './EnjuRW07FromStaList01';
