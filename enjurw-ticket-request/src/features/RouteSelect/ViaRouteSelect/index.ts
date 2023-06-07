b173bbbda3c3ced656e32c9278048712ca91a23c
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-07 16:40:24 +0900

    20230607 ADD

--- enjurw-ticket-request/src/features/RouteSelect/ViaRouteSelect/index.ts
+++ enjurw-ticket-request/src/features/RouteSelect/ViaRouteSelect/index.ts
@@ -2,3 +2,5 @@ export * from './ViaRouteSelectEnjuRW01';
 export * from './ViaRouteSelectEnjuRW02';
 export * from './ViaRouteSelectEnjuRW03';
 export * from './ViaRouteSelectEnjuRW04';
+export * from './ViaRouteSelectEnjuRW05';
+export * from './ViaRouteSelectEnjuRW06';
