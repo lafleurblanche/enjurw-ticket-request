bf3bc054b6feca4f028c8e1cc9f29352c3947206
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-07 16:39:56 +0900

    20230607 ADD

--- enjurw-ticket-request/src/features/RouteSelect/FromStaSelect/index.ts
+++ enjurw-ticket-request/src/features/RouteSelect/FromStaSelect/index.ts
@@ -2,3 +2,5 @@ export * from './FromStaSelectEnjuRW01';
 export * from './FromStaSelectEnjuRW02';
 export * from './FromStaSelectEnjuRW03';
 export * from './FromStaSelectEnjuRW04';
+export * from './FromStaSelectEnjuRW05';
+export * from './FromStaSelectEnjuRW06';
