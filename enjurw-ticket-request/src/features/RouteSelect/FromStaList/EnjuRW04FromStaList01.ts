dc92842b2cc9a2d3382beab44ac12d5f730ee14b
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-07 13:59:02 +0900

    20230607 NEW

---
+++ enjurw-ticket-request/src/features/RouteSelect/FromStaList/EnjuRW04FromStaList01.ts
@@ -0,0 +1,34 @@
+const EnjuRW04FromStaList01 = [
+  {
+    id: 1,
+    staCode: 'RW0401',
+    staName: '筏谷',
+    viaRouteName: '筏谷'
+  },
+  {
+    id: 2,
+    staCode: 'RW0402',
+    staName: '中筏谷',
+    viaRouteName: '筏谷'
+  },
+  {
+    id: 3,
+    staCode: 'RW0403',
+    staName: '蛞川',
+    viaRouteName: '筏谷'
+  },
+  {
+    id: 4,
+    staCode: 'RW0404',
+    staName: '',
+    viaRouteName: ''
+  },
+  {
+    id: 5,
+    staCode: 'RW0405',
+    staName: '',
+    viaRouteName: ''
+  },
+];
+
+export default EnjuRW04FromStaList01;
