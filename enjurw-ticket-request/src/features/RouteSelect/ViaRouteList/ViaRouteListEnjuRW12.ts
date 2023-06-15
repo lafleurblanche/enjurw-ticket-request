4d991b7880d1d764ce4221b78161780b195028ee
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-15 16:25:01 +0900

    20230615 新規作成

---
+++ enjurw-ticket-request/src/features/RouteSelect/ViaRouteList/ViaRouteListEnjuRW12.ts
@@ -0,0 +1,39 @@
+const ViaRouteListEnjuRW12 = [
+  {
+    id: 0,
+    routeID: 'RW12',
+    routeName: '自線内',
+    viaRouteName: '珩谷',
+    selectedRoute: '経珩'
+  },
+  {
+    id: 1,
+    routeID: 'RW99',
+    routeName: '',
+    viaRouteName: '',
+    selectedRoute: ''
+  },
+  {
+    id: 2,
+    routeID: 'RW99',
+    routeName: '',
+    viaRouteName: '',
+    selectedRoute: ''
+  },
+  {
+    id: 3,
+    routeID: 'RW99',
+    routeName: '',
+    viaRouteName: '',
+    selectedRoute: ''
+  },
+  {
+    id: 4,
+    routeID: 'RW99',
+    routeName: '',
+    viaRouteName: '',
+    selectedRoute: ''
+  },
+];
+
+export default ViaRouteListEnjuRW12;
