51a731aedaed149ce49b08434cd42d5daf75b932
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-15 14:15:44 +0900

    20230615 新規作成

---
+++ enjurw-ticket-request/src/features/RouteSelect/ViaRouteList/ViaRouteListEnjuRW08.ts
@@ -0,0 +1,39 @@
+const ViaRouteListEnjuRW08 = [
+  {
+    id: 0,
+    routeID: 'RW08',
+    routeName: '自線内',
+    viaRouteName: '笒谷',
+    selectedRoute: '経笒'
+  },
+  {
+    id: 1,
+    routeID: 'RW99',
+    routeName: '',
+    viaRouteName: '',
+    selectedRoute: ''
+  },
+  {
+    id: 2,
+    routeID: 'RW99',
+    routeName: '',
+    viaRouteName: '',
+    selectedRoute: ''
+  },
+  {
+    id: 3,
+    routeID: 'RW99',
+    routeName: '',
+    viaRouteName: '',
+    selectedRoute: ''
+  },
+  {
+    id: 4,
+    routeID: 'RW99',
+    routeName: '',
+    viaRouteName: '',
+    selectedRoute: ''
+  },
+];
+
+export default ViaRouteListEnjuRW08;
