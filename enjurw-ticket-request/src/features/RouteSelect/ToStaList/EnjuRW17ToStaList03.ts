466ee4d9da447b47c0c7ac90d9ed97b20de85b4c
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-07-01 19:41:19 +0900

    20230701 新規作成

---
+++ enjurw-ticket-request/src/features/RouteSelect/ToStaList/EnjuRW17ToStaList03.ts
@@ -0,0 +1,29 @@
+const EnjuRW17ToStaList03 = [
+  {
+    id: 1,
+    staCode: 'RW1711',
+    staName: '筮谷'
+  },
+  {
+    id: 2,
+    staCode: 'RW1712',
+    staName: '和瀬'
+  },
+  {
+    id: 3,
+    staCode: 'RW1713',
+    staName: '上和瀬'
+  },
+  {
+    id: 4,
+    staCode: 'RW1714',
+    staName: '卯塚'
+  },
+  {
+    id: 5,
+    staCode: 'RW1715',
+    staName: ''
+  },
+];
+
+export default EnjuRW17ToStaList03;
